e7b0e13ff98134a68c486b429c5687505e0954df
tpoenariu <tpoenariu@gmail.com>
2025-11-26 17:31:02 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Poenariu_ElenaTeodora_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Poenariu_ElenaTeodora_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
