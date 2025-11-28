b0b5ba0c82749925c730c031de33343c039f4305
tpoenariu <tpoenariu@gmail.com>
2025-11-28 23:32:06 +0200

    lab9_part1

---
+++ Models/ListProduct.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Poenariu_ElenaTeodora_Lab7.Models
+{
+    public class ListProduct
+    { 
+        [PrimaryKey, AutoIncrement] 
+        public int ID { get; set; } 
+        [ForeignKey(typeof(ShopList))] 
+        public int ShopListID { get; set; } 
+        public int ProductID { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Poenariu_ElenaTeodora_Lab7.Models
+{
+    public class Product
+    { 
+        [PrimaryKey, AutoIncrement] 
+        public int ID { get; set; } 
+        public string Description { get; set; } 
+        [OneToMany] public List<ListProduct> ListProducts { get; set; }
+    }
+}
