666acce068f8c57f15bd0820cb7aa8b4076f10d0
tpoenariu <tpoenariu@gmail.com>
2025-11-28 23:16:15 +0200

    Lab8 final

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,3 +1,5 @@
+using Poenariu_ElenaTeodora_Lab7.Models;
+
 namespace Poenariu_ElenaTeodora_Lab7;
 
 public partial class ListPage : ContentPage
@@ -6,4 +8,17 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
+async void OnSaveButtonClicked(object sender, EventArgs e)
+{ 
+	var slist = (ShopList)BindingContext;
+	slist.Date = DateTime.UtcNow;
+	await App.Database.SaveShopListAsync(slist); 
+	await Navigation.PopAsync();
+}
+async void OnDeleteButtonClicked(object sender, EventArgs e) 
+{ 
+	var slist = (ShopList)BindingContext; 
+	await App.Database.DeleteShopListAsync(slist);
+	await Navigation.PopAsync();
+}
 }
